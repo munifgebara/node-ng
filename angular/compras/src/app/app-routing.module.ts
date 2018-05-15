7ab30a3d80f4256970b8c5c1ad0278da157e372f
Munif Gebara Junior <munif@MBP-de-Munif.HotSpot300>
2018-05-15 01:26:36 -0300

    rotas

--- angular/compras/src/app/app-routing.module.ts
+++ angular/compras/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrudComponent as ProdutoComponet} from './produtos/crud/crud.component';
+import { CrudComponent as ListasComponet} from './listas/crud/crud.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produtos', component: ProdutoComponet },
+  { path: 'listas', component: ListasComponet },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: DashboardComponent }
+
+   
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/compras/src/app/app.module.ts
+++ angular/compras/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProdutosModule } from './produtos/produtos.module';
+import { ListasModule } from './listas/listas.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProdutosModule,
+    ListasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
