cff6a15e74d1c8f54bec8a109e0a874ac3df4b41
Munif Gebara Junior <munifgebara@gmail.com>
2018-05-19 09:37:20 -0300

    retirar do app module as subrotas

--- angular/rotas/src/app/app-routing.module.ts
+++ angular/rotas/src/app/app-routing.module.ts
@@ -3,14 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { OutraComponent } from './outra/outra.component';
 import { NaoExisteComponent } from './nao-existe/nao-existe.component';
-import { CrudComponent as CrudProdutos } from './produtos/crud/crud.component'; import { CrudComponent } from './produtos/crud/crud.component';
-import { CrudComponent as CrudListas } from './listas/crud/crud.component';
 
 const routes: Routes = [
   { path: 'principal', component: PrincipalComponent },
-  { path: 'produtos', component: CrudProdutos },
-  { path: 'listas', component: CrudListas },
-
 
   { path: 'outra', component: OutraComponent },
   { path: '', redirectTo: '/principal', pathMatch: 'full' },
